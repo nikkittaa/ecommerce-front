d7dab8524cdc75008531f990ebe4cc17758fc718
Nikita <nikitanb04@gmail.com>
2024-08-19 21:59:57 +0530

    added all products page

--- components/NewProducts.js
+++ components/NewProducts.js
@@ -1,17 +1,7 @@
 import styled from "styled-components";
 import Center from "./Center";
-import ProductBox from "./ProductBox";
+import ProductsGrid from "./ProductsGrid";
 
-const ProductsGrid = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    gap: 20px;
-    padding-top: 0px;
-
-    @media (max-width: 768px){
-        grid-template-columns: 1fr;
-    }
-`;
 
 const Title = styled.h2`
     font-size: 2rem;
@@ -21,12 +11,7 @@ export default function NewProducts({products}){
     return(
         <Center>
         <Title>New Arrivals</Title>
-            <ProductsGrid>
-            {products?.length > 0 && products.map(product => (
-                <ProductBox product = {product}/>
-            ))
-            }
-        </ProductsGrid>
+            <ProductsGrid products = {products}/>
         </Center>
     );
 }
